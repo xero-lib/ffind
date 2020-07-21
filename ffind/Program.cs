76132d1f2dacc91a820a2b53cb8a051d98a0082d
xero-lib <jonathankurtz@protonmail.com>
2020-07-21 18:10:49 -0500

    Added proc as a pruned path, slight verbosity improvement

--- ffind/Program.cs
+++ ffind/Program.cs
@@ -65,7 +65,7 @@ namespace ffind
                 Console.WriteLine();
                 Console.WriteLine("When searching it searches the ENTIRE path of the file, so keep that in mind.");
                 Console.WriteLine();
-                Console.WriteLine("Created by Krutonium - https://github.com/Krutonium");
+                Console.WriteLine("Created by Krutonium - https://github.com/Krutonium\nRevised by xero-lib - https://github.com/xero-lib");
                 Console.WriteLine("I borrowed some code for traversing the filesystem cleanly from Microsoft");
                 Console.WriteLine("https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/file-system/how-to-iterate-through-a-directory-tree");
             }
@@ -90,8 +90,8 @@ namespace ffind
         {
             if (cfg.caseSensitive == false)
             {
-                var toFindLower = toFind.ToLower();
-                foreach (var item in fileList)
+                toFindLower = toFind.ToLower();
+                foreach ( item in fileList)
                 {
                     if (item.ToLower().Contains(toFindLower))
                     {
@@ -101,7 +101,7 @@ namespace ffind
             }
             else
             {
-                foreach (var item in fileList)
+                foreach (item in fileList)
                 {
                     if (item.Contains(toFind))
                     {
@@ -113,10 +113,11 @@ namespace ffind
         
         public static void createConfig()
         {
+	//Currently only supported by Arch based operating systems
             cfg.PruneNames.Add(".git");
             cfg.PruneNames.Add(".hg");
             cfg.PruneNames.Add(".svn");
-            
+           //#########################// 
             cfg.PrunePaths.Add("/afs");
             cfg.PrunePaths.Add("/dev");
             cfg.PrunePaths.Add("/media");
@@ -125,13 +126,15 @@ namespace ffind
             cfg.PrunePaths.Add("/sfs");
             cfg.PrunePaths.Add("/tmp");
             cfg.PrunePaths.Add("/udev");
-            cfg.PrunePaths.Add("/var/cache");
-            cfg.PrunePaths.Add("/var/lib/pacman/local");
-            cfg.PrunePaths.Add("/var/lock");
-            cfg.PrunePaths.Add("/var/run");
-            cfg.PrunePaths.Add("/var/spool");
-            cfg.PrunePaths.Add("/var/tmp");
+            cfg.PrunePaths.Add("//cache");
+            cfg.PrunePaths.Add("//lock");
+            cfg.PrunePaths.Add("//run");
+            cfg.PrunePaths.Add("//spool");
+            cfg.PrunePaths.Add("/var/lib/pacman/local"),
+ 	    cfg.PrunePaths.Add("//tmp");
+	    cfg.PrunePaths.Add("//proc");
         }
+
         public static void UpdateDB()
         {
             //Start Indexing from root.
@@ -149,11 +152,11 @@ namespace ffind
             try
             {
                 files = root.GetFiles("*.*");
-            }
-            catch (UnauthorizedAccessException e)
-            {
+             catch (UnauthorizedAccessException e)
+           	 {
                 //We're not authorized, and that's fine.
-            }
+                Console.WriteLine("Cannot access {0}, Permission Denied, but this is expected.", e);
+		 }
             catch (System.IO.DirectoryNotFoundException e)
             {
                 Console.WriteLine(e.Message);
@@ -177,7 +180,7 @@ namespace ffind
                         {
                             try
                             {
-                                var att = File.GetAttributes(dirInfo.FullName);
+                                 att = File.GetAttributes(dirInfo.FullName);
                                 if (!att.HasFlag(FileAttributes.ReparsePoint))
                                 {
                                     //Console.WriteLine("Entering " + dirInfo.FullName);
@@ -203,23 +206,23 @@ namespace ffind
         {
         byte[] compressedBytes;
 
-        using (var uncompressedStream = new MemoryStream(Encoding.UTF8.GetBytes(uncompressedString)))
+        using ( uncompressedStream = new MemoryStream(Encoding.UTF8.GetBytes(uncompressedString)))
         {
-            using (var compressedStream = new MemoryStream())
+            using ( compressedStream = new MemoryStream())
             { 
                 // setting the leaveOpen parameter to true to ensure that compressedStream will not be closed when compressorStream is disposed
                 // this allows compressorStream to close and flush its buffers to compressedStream and guarantees that compressedStream.ToArray() can be called afterward
                 // although MSDN documentation states that ToArray() can be called on a closed MemoryStream, I don't want to rely on that very odd behavior should it ever change
                 if (cfg.shouldCompressDB)
                 {
-                    using (var compressorStream = new DeflateStream(compressedStream, CompressionLevel.Optimal, true))
+                    using ( compressorStream = new DeflateStream(compressedStream, CompressionLevel.Optimal, true))
                     {
                         uncompressedStream.CopyTo(compressorStream);
                     }
                 }
                 else
                 {
-                    using (var compressorStream = new DeflateStream(compressedStream, CompressionLevel.NoCompression, true))
+                    using ( compressorStream = new DeflateStream(compressedStream, CompressionLevel.NoCompression, true))
                     {
                         uncompressedStream.CopyTo(compressorStream);
                     }
@@ -242,11 +245,11 @@ namespace ffind
     {
         byte[] decompressedBytes;
 
-        var compressedStream = new MemoryStream(Convert.FromBase64String(compressedString));
+         compressedStream = new MemoryStream(Convert.FromBase64String(compressedString));
 
-        using (var decompressorStream = new DeflateStream(compressedStream, CompressionMode.Decompress))
+        using ( decompressorStream = new DeflateStream(compressedStream, CompressionMode.Decompress))
         {
-            using (var decompressedStream = new MemoryStream())
+            using ( decompressedStream = new MemoryStream())
             {
                 decompressorStream.CopyTo(decompressedStream);
 
